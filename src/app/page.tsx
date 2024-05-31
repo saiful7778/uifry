9973b2fd090de3dc7b3b555267bd5fe5deea6f05
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-06-01 02:13:15 +0600

    update button variants

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import Button from "@/components/ui/button";
 import React from "react";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Button>Click</Button>
+      <Button>Click</Button>
+    </div>
+  );
 }

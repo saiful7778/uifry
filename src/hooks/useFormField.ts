e7b482ac0f5e03ef2177dd94878c353087eccc3a
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-06-02 00:02:39 +0600

    add form component

---
+++ src/hooks/useFormField.ts
@@ -0,0 +1,26 @@
+import { FormFieldContext, FormItemContext } from "@/components/ui/form";
+import { useContext } from "react";
+import { useFormContext } from "react-hook-form";
+
+export default function useFormField() {
+  const fieldContext = useContext(FormFieldContext);
+  const itemContext = useContext(FormItemContext);
+  const { getFieldState, formState } = useFormContext();
+
+  const fieldState = getFieldState(fieldContext.name, formState);
+
+  if (!fieldContext) {
+    throw new Error("useFormField should be used within <FormField>");
+  }
+
+  const { id } = itemContext;
+
+  return {
+    id,
+    name: fieldContext.name,
+    formItemId: `${id}-form-item`,
+    formDescriptionId: `${id}-form-item-description`,
+    formMessageId: `${id}-form-item-message`,
+    ...fieldState,
+  };
+}

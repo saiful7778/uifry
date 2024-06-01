00c983b303928dfb03ff38969d94d04742c243b8
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-06-01 11:17:31 +0600

    add current theme changing logo

--- src/components/SiteLogo.tsx
+++ src/components/SiteLogo.tsx
@@ -2,11 +2,18 @@ import siteLogoLight from "../../public/images/site-logo-light.png";
 import siteLogoDark from "../../public/images/site-logo-dark.png";
 import Image from "next/image";
 import Link from "next/link";
+import { useTheme } from "next-themes";
 
 export default function SiteLogo() {
+  const { theme } = useTheme();
+
   return (
     <Link href="/">
-      <Image src={siteLogoLight} alt="uifry logo" />
+      {theme === "dark" ? (
+        <Image src={siteLogoDark} alt="uifry logo" />
+      ) : (
+        <Image src={siteLogoLight} alt="uifry logo" />
+      )}
     </Link>
   );
 }

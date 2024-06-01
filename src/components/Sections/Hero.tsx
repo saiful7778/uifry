2ce13dced8ec983ba7a4f911ea07fe10a1231876
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-06-01 20:20:11 +0600

    update all star component and others

--- src/components/Sections/Hero.tsx
+++ src/components/Sections/Hero.tsx
@@ -4,26 +4,20 @@ import Image from "next/image";
 import HeroImage from "../../../public/images/hero-phone-image.png";
 import HeroBgImage from "../../../public/images/hero-phone-bg-image.png";
 import HeroElement from "../../../public/images/hero-element.png";
-import star from "../../../public/images/star.png";
 import Button from "../ui/button";
 import Link from "next/link";
+import Star from "../Star";
 
 export default function Hero() {
   return (
-    <div className="relative my-4 flex flex-col justify-center md:flex-row">
-      <Image
-        className="star absolute left-0 top-0 z-[-1]"
-        src={star}
-        alt="star image"
-      />
-      <Image
-        className="star absolute right-0 top-0 z-[-1]"
-        src={star}
-        alt="star image"
-      />
+    <section className="relative my-4 flex flex-col justify-center md:flex-row">
+      <Star className="left-0 top-0" />
+      <Star className="right-0 top-0" />
       <div className="relative mt-20 w-full max-md:order-2 md:w-1/2">
         <Blob className="-top-5 right-56" />
-        <h1 className="text-7xl font-bold">{jsonData.hero.headline}</h1>
+        <h1 className="text-6xl font-bold md:text-7xl">
+          {jsonData.hero.headline}
+        </h1>
         <p className="my-4 font-medium text-details">
           {jsonData.hero.subheadline}
         </p>
@@ -39,11 +33,7 @@ export default function Hero() {
             </Link>
           </Button>
         </div>
-        <Image
-          className="star absolute bottom-1/2 left-1/3 z-[-1]"
-          src={star}
-          alt="star image"
-        />
+        <Star className="bottom-1/2 left-1/3" />
         <Image src={HeroElement} alt="Hero element" />
       </div>
       <div className="relative w-full max-md:order-1 md:w-2/5">
@@ -56,12 +46,8 @@ export default function Hero() {
           />
           <Blob className="bottom-14 right-56" />
         </div>
-        <Image
-          className="star absolute bottom-1/2 left-0 z-[-1]"
-          src={star}
-          alt="star image"
-        />
+        <Star className="bottom-1/2 left-0" />
       </div>
-    </div>
+    </section>
   );
 }

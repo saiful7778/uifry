0d117f5b1ae47e26b9514d34a91abf1f04634e6a
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-06-01 11:17:10 +0600

    add theme changer component

---
+++ src/components/ThemeChange.tsx
@@ -0,0 +1,32 @@
+"use client";
+import DropdownMenu from "./ui/dropdown-menu";
+import Button from "./ui/button";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+
+export default function ThemeChange() {
+  const { setTheme } = useTheme();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenu.trigger asChild>
+        <Button size="icon">
+          <Sun className="size-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute size-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenu.trigger>
+      <DropdownMenu.content align="end">
+        <DropdownMenu.item onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenu.item>
+        <DropdownMenu.item onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenu.item>
+        <DropdownMenu.item onClick={() => setTheme("system")}>
+          System
+        </DropdownMenu.item>
+      </DropdownMenu.content>
+    </DropdownMenu>
+  );
+}

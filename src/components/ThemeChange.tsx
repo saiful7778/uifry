11b32c0eb51e29ef7a4f300c9a374c473e722acb
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-06-02 00:03:45 +0600

    add tooltip

--- src/components/ThemeChange.tsx
+++ src/components/ThemeChange.tsx
@@ -10,22 +10,22 @@ export default function ThemeChange() {
 
   return (
     <DropdownMenu>
-      <DropdownMenu.trigger asChild>
-        <Tooltip.provider>
-          <Tooltip>
-            <Tooltip.trigger asChild>
+      <Tooltip.provider>
+        <Tooltip>
+          <Tooltip.trigger asChild>
+            <DropdownMenu.trigger asChild>
               <Button size="icon">
                 <Sun className="size-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
                 <Moon className="absolute size-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
                 <span className="sr-only">Toggle theme</span>
               </Button>
-            </Tooltip.trigger>
-            <Tooltip.content>
-              <p>Change theme</p>
-            </Tooltip.content>
-          </Tooltip>
-        </Tooltip.provider>
-      </DropdownMenu.trigger>
+            </DropdownMenu.trigger>
+          </Tooltip.trigger>
+          <Tooltip.content>
+            <p>Change theme</p>
+          </Tooltip.content>
+        </Tooltip>
+      </Tooltip.provider>
       <DropdownMenu.content align="end">
         <DropdownMenu.item onClick={() => setTheme("dark")}>
           Dark

a84027a4587084c4812f2c7ad422852be992f2f9
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-06-02 15:50:39 +0600

    add unsplash image source

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "via.placeholder.com",
       },
+      {
+        protocol: "https",
+        hostname: "source.unsplash.com",
+      },
     ],
   },
 };

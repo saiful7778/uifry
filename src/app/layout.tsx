2929d6f45cbb9f44dbe1afafad5e8d4f996e34dd
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-06-11 14:42:09 +0600

    add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,6 +35,23 @@ const clashDisplayFont = localFont({
 
 export const metadata: Metadata = {
   title: "Uifry",
+  authors: [
+    {
+      name: "Saiful Islam",
+      url: "https://www.linkedin.com/in/saiful-islam-0471b924b",
+    },
+  ],
+  robots: {
+    index: true,
+    follow: true,
+  },
+  openGraph: {
+    title: "Uifry",
+    description: "This is Uifry app website",
+    images:
+      "https://github-production-user-asset-6210df.s3.amazonaws.com/83330293/335883168-96cefa20-3135-4cad-a2e6-8ae3223dc030.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVCODYLSA53PQK4ZA%2F20240611%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240611T083749Z&X-Amz-Expires=300&X-Amz-Signature=64ee51d55ab82779e436b77c7b305f5d9d2d25faa45a477462eb96e3d627fe44&X-Amz-SignedHeaders=host&actor_id=83330293&key_id=0&repo_id=808764902",
+  },
+  description: "This is Uifry app website",
 };
 
 export default function RootLayout({
